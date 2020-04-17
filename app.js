f6e9ae7a2a7f14b479dfd651d736f57551b041bf
Marian Silviu Talmacel <mariansilviutalmacel@Marians-MacBook-Pro.local>
2020-04-17 21:00:03 +0100

    small chnage

--- app.js
+++ app.js
@@ -15,10 +15,11 @@ document.addEventListener("DOMContentLoaded", () => {
       this.y = y;
       this.owner = null;
     }
-    getDirection(dir, grid, owner) {
+    getDirection(dir, grid, owner, all) {
       let next = (grid[this.x + dir[0]] || [])[this.y + dir[1]];
       if (!next || owner !== next.owner) return 0;
-      return 1 + next.getDirection(dir, grid, owner);
+      all.push(next);
+      return 1 + next.getDirection(dir, grid, owner, all);
     }
   }
   let data = new Array(6).fill(1).map((e) => new Array(7));
@@ -49,21 +50,58 @@ document.addEventListener("DOMContentLoaded", () => {
             let x = Number(squares[index].getAttribute("x"));
             let y = Number(squares[index].getAttribute("y"));
             data[x][y].owner = "player-one";
-            let top = data[x][y].getDirection([1, 0], data, "player-one");
-            let bottom = data[x][y].getDirection([-1, 0], data, "player-one");
-            let left = data[x][y].getDirection([0, 1], data, "player-one");
-            let right = data[x][y].getDirection([0, -1], data, "player-one");
-            let topLeft = data[x][y].getDirection([1, 1], data, "player-one");
+            let vertical = [data[x][y]];
+            let horizontal = [data[x][y]];
+            let firstDiagonal = [data[x][y]];
+            let secondDiagonal = [data[x][y]];
+
+            let top = data[x][y].getDirection(
+              [1, 0],
+              data,
+              "player-one",
+              vertical
+            );
+            let bottom = data[x][y].getDirection(
+              [-1, 0],
+              data,
+              "player-one",
+              vertical
+            );
+            let left = data[x][y].getDirection(
+              [0, 1],
+              data,
+              "player-one",
+              horizontal
+            );
+            let right = data[x][y].getDirection(
+              [0, -1],
+              data,
+              "player-one",
+              horizontal
+            );
+            let topLeft = data[x][y].getDirection(
+              [1, 1],
+              data,
+              "player-one",
+              firstDiagonal
+            );
             let bottomRight = data[x][y].getDirection(
               [-1, -1],
               data,
-              "player-one"
+              "player-one",
+              firstDiagonal
             );
-            let topRight = data[x][y].getDirection([1, -1], data, "player-one");
+            let topRight = data[x][y].getDirection(
+              [1, -1],
+              data,
+              "player-one",
+              secondDiagonal
+            );
             let bottomLeft = data[x][y].getDirection(
               [-1, 1],
               data,
-              "player-one"
+              "player-one",
+              secondDiagonal
             );
             console.log(
               top + bottom + 1,
@@ -71,6 +109,7 @@ document.addEventListener("DOMContentLoaded", () => {
               topLeft + bottomRight + 1,
               topRight + bottomLeft + 1
             );
+            console.log(vertical, horizontal, firstDiagonal, secondDiagonal);
           } else if (currentPlayer === 2) {
             playerTwoWins.push(index);
             square.classList.add("taken");
@@ -93,79 +132,7 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
   });
-  // function checkBoard() {
-  //   if (resetTimeout) return;
-  //   const winningArrays = [
-  //     [0, 1, 2, 3],
-  //     [41, 40, 39, 38],
-  //     [7, 8, 9, 10],
-  //     [34, 33, 32, 31],
-  //     [14, 15, 16, 17],
-  //     [27, 26, 25, 24],
-  //     [21, 22, 23, 24],
-  //     [20, 19, 18, 17],
-  //     [28, 29, 30, 31],
-  //     [13, 12, 11, 10],
-  //     [35, 36, 37, 38],
-  //     [6, 5, 4, 3],
-  //     [0, 7, 14, 21],
-  //     [41, 34, 27, 20],
-  //     [1, 8, 15, 22],
-  //     [40, 33, 26, 19],
-  //     [2, 9, 16, 23],
-  //     [39, 32, 25, 18],
-  //     [3, 10, 17, 24],
-  //     [38, 31, 24, 17],
-  //     [4, 11, 18, 25],
-  //     [37, 30, 23, 16],
-  //     [5, 12, 19, 26],
-  //     [36, 29, 22, 15],
-  //     [6, 13, 20, 27],
-  //     [35, 28, 21, 14],
-  //     [0, 8, 16, 24],
-  //     [41, 33, 25, 17],
-  //     [7, 15, 23, 31],
-  //     [34, 26, 18, 10],
-  //     [14, 22, 30, 38],
-  //     [27, 19, 11, 3],
-  //     [35, 29, 23, 17],
-  //     [6, 12, 18, 24],
-  //     [28, 22, 16, 10],
-  //     [13, 19, 25, 31],
-  //     [21, 15, 9, 3],
-  //     [20, 26, 32, 38],
-  //     [36, 30, 24, 18],
-  //     [5, 11, 17, 23],
-  //     [37, 31, 25, 19],
-  //     [4, 10, 16, 22],
-  //     [2, 10, 18, 26],
-  //     [39, 31, 23, 15],
-  //     [1, 9, 17, 25],
-  //     [40, 32, 24, 16],
-  //     [9, 7, 25, 33],
-  //     [8, 16, 24, 32],
-  //     [11, 7, 23, 29],
-  //     [12, 18, 24, 30],
-  //     [1, 2, 3, 4],
-  //     [5, 4, 3, 2],
-  //     [8, 9, 10, 11],
-  //     [12, 11, 10, 9],
-  //     [15, 16, 17, 18],
-  //     [19, 18, 17, 16],
-  //     [22, 23, 24, 25],
-  //     [26, 25, 24, 23],
-  //     [29, 30, 31, 32],
-  //     [33, 32, 31, 30],
-  //     [36, 37, 38, 39],
-  //     [40, 39, 38, 37],
-  //     [7, 14, 21, 28],
-  //     [8, 15, 22, 29],
-  //     [9, 16, 23, 30],
-  //     [10, 17, 24, 31],
-  //     [11, 18, 25, 32],
-  //     [12, 19, 26, 33],
-  //     [13, 20, 27, 34],
-  //   ];
+
   //   for (let y = 0; y < winningArrays.length; y++) {
   //     const square1 = squares[winningArrays[y][0]];
   //     const square2 = squares[winningArrays[y][1]];
