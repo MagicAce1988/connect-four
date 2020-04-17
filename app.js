efb40a809c0bf86de259bc1dbd36993f9cffc96c
Marian Silviu Talmacel <mariansilviutalmacel@Marians-MacBook-Pro.local>
2020-04-18 00:09:30 +0100

    nice changes

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const squares = document.querySelectorAll(".grid div");
+  const grid = document.querySelector(".grid");
+  var squares = document.querySelectorAll(".grid div");
   const result = document.querySelector("#result");
   const displayCurrentPlayer = document.querySelector("#current-player");
   const error = document.querySelector(".error");
   const playerColor = document.querySelector(".player-color");
-  const playerOneWins = [];
-  const playerTwoWins = [];
   let currentPlayer = 1;
-  let resetTimeout;
+  const submit = document.querySelector(".submit");
+  let heightValue, widthValue;
+
   class Cell {
     constructor(x, y, ref) {
       this.ref = ref;
@@ -22,6 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
       return 1 + next.getDirection(dir, grid, owner, all);
     }
   }
+
   let data = new Array(6).fill(1).map((e) => new Array(7));
   for (let index = 0; index < squares.length - 7; index++) {
     const ref = squares[index];
@@ -31,7 +33,160 @@ document.addEventListener("DOMContentLoaded", () => {
     ref.setAttribute("y", y);
     data[x][y] = new Cell(x, y, ref); //{ ref };
   }
-  console.table(data);
+
+  const checkWin = (index, player) => {
+    const displayWin = (
+      verticalSum,
+      horizontalSum,
+      firstDiagonalSum,
+      secondDiagonalSum,
+      player
+    ) => {
+      let winDirection;
+
+      switch (true) {
+        case verticalSum === 4:
+          winDirection = vertical;
+          break;
+        case horizontalSum === 4:
+          winDirection = vertical;
+          break;
+        case firstDiagonalSum === 4:
+          winDirection = vertical;
+          break;
+        case secondDiagonalSum === 4:
+          winDirection = vertical;
+          break;
+        default:
+          winDirection = [];
+      }
+
+      if (winDirection.length > 0) {
+        result.innerHTML = player === 1 ? "Player One Wins" : "Player Two wins";
+        error.style.display = "none";
+      }
+    };
+
+    let passedPlayer = player === 1 ? "player-one" : "player-two";
+    let x = Number(squares[index].getAttribute("x"));
+    let y = Number(squares[index].getAttribute("y"));
+    data[x][y].owner = passedPlayer;
+    let vertical = [data[x][y]];
+    let horizontal = [data[x][y]];
+    let firstDiagonal = [data[x][y]];
+    let secondDiagonal = [data[x][y]];
+
+    let top = data[x][y].getDirection([1, 0], data, passedPlayer, vertical);
+    let bottom = data[x][y].getDirection([-1, 0], data, passedPlayer, vertical);
+    let left = data[x][y].getDirection([0, 1], data, passedPlayer, horizontal);
+    let right = data[x][y].getDirection(
+      [0, -1],
+      data,
+      passedPlayer,
+      horizontal
+    );
+    let topLeft = data[x][y].getDirection(
+      [1, 1],
+      data,
+      passedPlayer,
+      firstDiagonal
+    );
+    let bottomRight = data[x][y].getDirection(
+      [-1, -1],
+      data,
+      passedPlayer,
+      firstDiagonal
+    );
+    let topRight = data[x][y].getDirection(
+      [1, -1],
+      data,
+      passedPlayer,
+      secondDiagonal
+    );
+    let bottomLeft = data[x][y].getDirection(
+      [-1, 1],
+      data,
+      passedPlayer,
+      secondDiagonal
+    );
+    displayWin(
+      top + bottom + 1,
+      left + right + 1,
+      topLeft + bottomRight + 1,
+      topRight + bottomLeft + 1,
+      player
+    );
+  };
+
+  submit.addEventListener("click", () => {
+    result.innerHTML = "";
+    const height = document.querySelector(".height");
+    const width = document.querySelector(".width");
+    heightValue = Number(height.value) || 7;
+    widthValue = Number(width.value) || 7;
+    while (grid.firstChild && grid.removeChild(grid.firstChild));
+    grid.style.height = `${heightValue * 40}px`;
+    grid.style.width = `${widthValue * 40}px`;
+    for (i = 0; i < heightValue * widthValue; i++) {
+      let div = document.createElement("div");
+      div.classList.add("cell");
+      grid.appendChild(div);
+    }
+    for (i = 0; i < widthValue; i++) {
+      let div = document.createElement("div");
+      div.classList.add("taken");
+      div.classList.add("cell");
+      grid.appendChild(div);
+    }
+    squares = document.querySelectorAll(".grid div");
+
+    data = new Array(heightValue).fill(1).map((e) => new Array(widthValue));
+    for (let index = 0; index < squares.length - widthValue; index++) {
+      const ref = squares[index];
+      let x = parseInt(index / widthValue);
+      let y = index % widthValue;
+      ref.setAttribute("x", x);
+      ref.setAttribute("y", y);
+      data[x][y] = new Cell(x, y, ref); //{ ref };
+    }
+
+    squares.forEach((square, index) => {
+      square.addEventListener("click", () => {
+        if (
+          squares[index + widthValue].classList.contains("taken") &&
+          !square.classList.contains("taken")
+        ) {
+          if (result.innerHTML.length < 1) {
+            if (currentPlayer === 1) {
+              square.classList.add("taken");
+              square.classList.add("player-one");
+              playerColor.classList.remove("player-one");
+              playerColor.classList.add("player-two");
+              checkWin(index, currentPlayer);
+              currentPlayer = 2;
+              displayCurrentPlayer.innerHTML = currentPlayer;
+            } else if (currentPlayer === 2) {
+              square.classList.add("taken");
+              square.classList.add("player-two");
+              playerColor.classList.remove("player-two");
+              playerColor.classList.add("player-one");
+              checkWin(index, currentPlayer);
+              currentPlayer = 1;
+              displayCurrentPlayer.innerHTML = currentPlayer;
+            }
+          }
+        } else {
+          if (result.innerHTML.length < 1) {
+            error.textContent = `You can't go there!`;
+            setTimeout(() => {
+              error.textContent = "";
+            }, 1000);
+          }
+        }
+      });
+    });
+  });
+
   squares.forEach((square, index) => {
     square.addEventListener("click", () => {
       if (
@@ -40,86 +195,21 @@ document.addEventListener("DOMContentLoaded", () => {
       ) {
         if (result.innerHTML.length < 1) {
           if (currentPlayer === 1) {
-            playerOneWins.push(index);
             square.classList.add("taken");
             square.classList.add("player-one");
             playerColor.classList.remove("player-one");
             playerColor.classList.add("player-two");
+            checkWin(index, currentPlayer);
             currentPlayer = 2;
             displayCurrentPlayer.innerHTML = currentPlayer;
-            let x = Number(squares[index].getAttribute("x"));
-            let y = Number(squares[index].getAttribute("y"));
-            data[x][y].owner = "player-one";
-            let vertical = [data[x][y]];
-            let horizontal = [data[x][y]];
-            let firstDiagonal = [data[x][y]];
-            let secondDiagonal = [data[x][y]];
-
-            let top = data[x][y].getDirection(
-              [1, 0],
-              data,
-              "player-one",
-              vertical
-            );
-            let bottom = data[x][y].getDirection(
-              [-1, 0],
-              data,
-              "player-one",
-              vertical
-            );
-            let left = data[x][y].getDirection(
-              [0, 1],
-              data,
-              "player-one",
-              horizontal
-            );
-            let right = data[x][y].getDirection(
-              [0, -1],
-              data,
-              "player-one",
-              horizontal
-            );
-            let topLeft = data[x][y].getDirection(
-              [1, 1],
-              data,
-              "player-one",
-              firstDiagonal
-            );
-            let bottomRight = data[x][y].getDirection(
-              [-1, -1],
-              data,
-              "player-one",
-              firstDiagonal
-            );
-            let topRight = data[x][y].getDirection(
-              [1, -1],
-              data,
-              "player-one",
-              secondDiagonal
-            );
-            let bottomLeft = data[x][y].getDirection(
-              [-1, 1],
-              data,
-              "player-one",
-              secondDiagonal
-            );
-            console.log(
-              top + bottom + 1,
-              left + right + 1,
-              topLeft + bottomRight + 1,
-              topRight + bottomLeft + 1
-            );
-            console.log(vertical, horizontal, firstDiagonal, secondDiagonal);
           } else if (currentPlayer === 2) {
-            playerTwoWins.push(index);
             square.classList.add("taken");
             square.classList.add("player-two");
             playerColor.classList.remove("player-two");
             playerColor.classList.add("player-one");
+            checkWin(index, currentPlayer);
             currentPlayer = 1;
             displayCurrentPlayer.innerHTML = currentPlayer;
-            let x2 = Number(squares[index].getAttribute("x"));
-            let y2 = Number(squares[index].getAttribute("y"));
           }
         }
       } else {
@@ -132,56 +222,4 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
   });
-
-  //   for (let y = 0; y < winningArrays.length; y++) {
-  //     const square1 = squares[winningArrays[y][0]];
-  //     const square2 = squares[winningArrays[y][1]];
-  //     const square3 = squares[winningArrays[y][2]];
-  //     const square4 = squares[winningArrays[y][3]];
-  //     if (
-  //       square1.classList.contains("player-one") &&
-  //       square2.classList.contains("player-one") &&
-  //       square3.classList.contains("player-one") &&
-  //       square4.classList.contains("player-one")
-  //     ) {
-  //       error.style.display = "none";
-  //       result.innerHTML = "Player one wins!";
-  //       currentPlayer = 2;
-  //       displayCurrentPlayer.innerHTML = currentPlayer;
-  //       resetTimeout = setTimeout(() => {
-  //         result.innerHTML = "";
-  //         squares.forEach((square, index) => {
-  //           resetTimeout = null;
-  //           square.classList.remove("taken", "player-one", "player-two");
-  //           if (index > squares.length - 8) {
-  //             square.classList.add("taken");
-  //           }
-  //           error.style.display = "block";
-  //         });
-  //       }, 3000);
-  //     } else if (
-  //       square1.classList.contains("player-two") &&
-  //       square2.classList.contains("player-two") &&
-  //       square3.classList.contains("player-two") &&
-  //       square4.classList.contains("player-two")
-  //     ) {
-  //       error.style.display = "none";
-  //       result.innerHTML = "Player two wins!";
-  //       currentPlayer = 1;
-  //       displayCurrentPlayer.innerHTML = currentPlayer;
-  //       resetTimeout = setTimeout(() => {
-  //         resetTimeout = null;
-  //         result.innerHTML = "";
-  //         squares.forEach((square, index) => {
-  //           square.classList.remove("taken", "player-one", "player-two");
-  //           if (index > squares.length - 8{
-  //             square.classList.add("taken");
-  //           }
-  //           error.style.display = "block";
-  //         });
-  //       }, 3000);
-  //     }
-  //   }
-  // }
-  // squares.forEach((square) => square.addEventListener("click", checkBoard));
 });
