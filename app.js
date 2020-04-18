2a396ca793ca36d82dc5ce487787313f61c43fcc
Marian Silviu Talmacel <mariansilviutalmacel@Marians-MacBook-Pro.local>
2020-04-18 18:27:36 +0100

    current version

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const displayCurrentPlayer = document.querySelector("#current-player");
   const error = document.querySelector(".error");
   const playerColor = document.querySelector(".player-color");
-  let currentPlayer = 1;
   const submit = document.querySelector(".submit");
-  let heightValue, widthValue, squares, data;
+  const alert = document.querySelector(".alert");
+  let heightValue, widthValue, squares, data, animateInterval;
+  let currentPlayer = 1;
 
   class Cell {
     constructor(x, y, ref) {
@@ -23,53 +24,15 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  const runGame = (gridWidth, gridHeight) => {
-    squares = document.querySelectorAll(".grid div");
-    data = new Array(gridHeight).fill(1).map((e) => new Array(gridWidth));
-    for (let index = 0; index < squares.length - gridWidth; index++) {
-      const ref = squares[index];
-      let x = parseInt(index / gridWidth);
-      let y = index % gridWidth;
-      ref.setAttribute("x", x);
-      ref.setAttribute("y", y);
-      data[x][y] = new Cell(x, y, ref); //{ ref };
-    }
-
-    squares.forEach((square, index) => {
-      square.addEventListener("click", () => {
-        if (
-          squares[index + gridWidth].classList.contains("taken") &&
-          !square.classList.contains("taken")
-        ) {
-          if (result.innerHTML.length < 1) {
-            if (currentPlayer === 1) {
-              square.classList.add("taken");
-              square.classList.add("player-one");
-              playerColor.classList.remove("player-one");
-              playerColor.classList.add("player-two");
-              checkWin(index, currentPlayer);
-              currentPlayer = 2;
-              displayCurrentPlayer.innerHTML = currentPlayer;
-            } else if (currentPlayer === 2) {
-              square.classList.add("taken");
-              square.classList.add("player-two");
-              playerColor.classList.remove("player-two");
-              playerColor.classList.add("player-one");
-              checkWin(index, currentPlayer);
-              currentPlayer = 1;
-              displayCurrentPlayer.innerHTML = currentPlayer;
-            }
-          }
-        } else {
-          if (result.innerHTML.length < 1) {
-            error.textContent = `You can't go there!`;
-            setTimeout(() => {
-              error.textContent = "";
-            }, 1000);
-          }
-        }
+  const animateWin = (winSet) => {
+    animateInterval = setInterval(() => {
+      winSet.forEach((win) => {
+        win.ref.style.opacity = 0.4;
+        setTimeout(() => {
+          win.ref.style.opacity = 1;
+        }, 500);
       });
-    });
+    }, 1000);
   };
 
   const checkWin = (index, player) => {
@@ -88,13 +51,13 @@ document.addEventListener("DOMContentLoaded", () => {
           winDirection = vertical;
           break;
         case horizontalSum === 4:
-          winDirection = vertical;
+          winDirection = horizontal;
           break;
         case firstDiagonalSum === 4:
-          winDirection = vertical;
+          winDirection = firstDiagonal;
           break;
         case secondDiagonalSum === 4:
-          winDirection = vertical;
+          winDirection = secondDiagonal;
           break;
         default:
           winDirection = [];
@@ -103,6 +66,7 @@ document.addEventListener("DOMContentLoaded", () => {
       if (winDirection.length > 0) {
         result.innerHTML = player === 1 ? "Player One Wins" : "Player Two wins";
         error.style.display = "none";
+        animateWin(winDirection);
       }
     };
 
@@ -157,50 +121,113 @@ document.addEventListener("DOMContentLoaded", () => {
     );
   };
 
+  const runGame = (gridWidth, gridHeight) => {
+    clearInterval(animateInterval);
+    currentPlayer = 1;
+    squares = document.querySelectorAll(".grid div");
+    data = new Array(gridHeight).fill(1).map((e) => new Array(gridWidth));
+    for (let index = 0; index < squares.length - gridWidth; index++) {
+      const ref = squares[index];
+      let x = parseInt(index / gridWidth);
+      let y = index % gridWidth;
+      ref.setAttribute("x", x);
+      ref.setAttribute("y", y);
+      data[x][y] = new Cell(x, y, ref); //{ ref };
+    }
+
+    squares.forEach((square, index) => {
+      square.addEventListener("click", () => {
+        if (
+          squares[index + gridWidth].classList.contains("taken") &&
+          !square.classList.contains("taken")
+        ) {
+          if (result.innerHTML.length < 1) {
+            if (currentPlayer === 1) {
+              square.classList.add("taken");
+              square.classList.add("player-one");
+              playerColor.classList.remove("player-one");
+              playerColor.classList.add("player-two");
+              checkWin(index, currentPlayer);
+              currentPlayer = 2;
+              displayCurrentPlayer.innerHTML = currentPlayer;
+            } else if (currentPlayer === 2) {
+              square.classList.add("taken");
+              square.classList.add("player-two");
+              playerColor.classList.remove("player-two");
+              playerColor.classList.add("player-one");
+              checkWin(index, currentPlayer);
+              currentPlayer = 1;
+              displayCurrentPlayer.innerHTML = currentPlayer;
+            }
+          }
+        } else {
+          if (result.innerHTML.length < 1) {
+            error.textContent = `You can't go there!`;
+            setTimeout(() => {
+              error.textContent = "";
+            }, 1000);
+          }
+        }
+      });
+    });
+  };
+
   submit.addEventListener("click", () => {
     result.innerHTML = "";
     const height = document.querySelector(".height");
     const width = document.querySelector(".width");
-    heightValue = Number(height.value) || 7;
-    widthValue = Number(width.value) || 7;
-    while (grid.firstChild && grid.removeChild(grid.firstChild));
-    grid.style.height =
-      heightValue > 10 || widthValue > 10
-        ? heightValue > 20 || widthValue > 20
-          ? `${heightValue * 10}px`
-          : `${heightValue * 20}px`
-        : `${heightValue * 40}px`;
-    grid.style.width =
-      heightValue > 10 || widthValue > 10
-        ? heightValue > 20 || widthValue > 20
-          ? `${widthValue * 10}px`
-          : `${widthValue * 20}px`
-        : `${widthValue * 40}px`;
-    for (i = 0; i < heightValue * widthValue; i++) {
-      let div = document.createElement("div");
-      div.classList.add(
-        heightValue > 10 || widthValue > 10
-          ? heightValue > 20 || widthValue > 20
-            ? "verySmallCell"
-            : "smallCell"
-          : "cell"
-      );
-      grid.appendChild(div);
-    }
-    for (i = 0; i < widthValue; i++) {
-      let div = document.createElement("div");
-      div.classList.add("taken");
-      div.classList.add(
-        heightValue > 10 || widthValue > 10
-          ? heightValue > 20 || widthValue > 20
-            ? "verySmallCell"
-            : "smallCell"
-          : "cell"
-      );
-      grid.appendChild(div);
-    }
+    if (Number(height.value) < 4 || Number(width.value) < 4) {
+      height.value = null;
+      width.value = null;
+      alert.style.height = "36px";
+      alert.style.padding = "8px";
+    } else {
+      alert.style.height = "0";
+      alert.style.padding = "0";
+      heightValue = Number(height.value);
+      widthValue = Number(width.value);
+      height.value = null;
+      width.value = null;
 
-    runGame(widthValue, heightValue);
+      while (grid.firstChild && grid.removeChild(grid.firstChild));
+      grid.style.height =
+        heightValue > 10 || widthValue > 10
+          ? heightValue > 20 || widthValue > 20
+            ? `${heightValue * 10}px`
+            : `${heightValue * 20}px`
+          : `${heightValue * 40}px`;
+      grid.style.width =
+        heightValue > 10 || widthValue > 10
+          ? heightValue > 20 || widthValue > 20
+            ? `${widthValue * 10}px`
+            : `${widthValue * 20}px`
+          : `${widthValue * 40}px`;
+      for (i = 0; i < heightValue * widthValue; i++) {
+        let div = document.createElement("div");
+        div.classList.add(
+          heightValue > 10 || widthValue > 10
+            ? heightValue > 20 || widthValue > 20
+              ? "verySmallCell"
+              : "smallCell"
+            : "cell"
+        );
+        grid.appendChild(div);
+      }
+      for (i = 0; i < widthValue; i++) {
+        let div = document.createElement("div");
+        div.classList.add("taken");
+        div.classList.add(
+          heightValue > 10 || widthValue > 10
+            ? heightValue > 20 || widthValue > 20
+              ? "verySmallCell"
+              : "smallCell"
+            : "cell"
+        );
+        grid.appendChild(div);
+      }
+
+      runGame(widthValue, heightValue);
+    }
   });
-  runGame(7, 6);
+  runGame(7, 7);
 });
